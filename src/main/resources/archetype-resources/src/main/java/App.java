f1b095d7d41af6ee070b94dd997acb1ef2301188
shoyencode <paul.desandes@free.fr>
2022-11-01 11:11:19 +0100

    first commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.shoyencode.bluefog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
